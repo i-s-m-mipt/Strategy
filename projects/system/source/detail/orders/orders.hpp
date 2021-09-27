e50e5842c1fc09b53f812bea96b917dfd1045d00
Ivan <i.s.m.mipt@yandex.ru>
2021-09-28 00:35:19 +0300

    Добавлена заготовка для разработки структуры стакана заявок.

---
+++ projects/system/source/detail/orders/orders.hpp
@@ -0,0 +1,26 @@
+#ifndef SOLUTION_SYSTEM_DETAIL_ORDERS_HPP
+#define SOLUTION_SYSTEM_DETAIL_ORDERS_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#  pragma once
+#endif // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include "../detail.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace detail
+		{
+			// TODO
+
+		} // namespace detail
+
+	} // namespace system
+
+} // namespace solution
+
+#endif // #ifndef SOLUTION_SYSTEM_DETAIL_ORDERS_HPP
