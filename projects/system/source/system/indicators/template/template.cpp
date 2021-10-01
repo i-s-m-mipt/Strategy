f33dfb8d2d3803c0c38e0bea9fa9ee108373d838
Ivan <i.s.m.mipt@yandex.ru>
2021-10-01 12:58:07 +0300

    Доработан и перенесен в другой раздел шаблон класса для реализации индикаторов.

---
+++ projects/system/source/system/indicators/template/template.cpp
@@ -0,0 +1,44 @@
+#include "template.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace indicators
+		{
+			void TEMPLATE::initialize() const
+			{
+				LOGGER(logger);
+
+				try
+				{
+					if (m_timesteps == 0ULL)
+					{
+						throw std::domain_error("required: (timesteps > 0)");
+					}
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < indicator_exception > (logger, exception);
+				}
+			}
+
+			void TEMPLATE::operator()(inputs_container_t & inputs) const
+			{
+				LOGGER(logger);
+
+				try
+				{
+					// ...
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < indicator_exception > (logger, exception);
+				}
+			}
+
+		} // namespace indicators
+
+	} // namespace system
+
+} // namespace solution
---
+++ projects/system/source/system/indicators/template/template.hpp
@@ -0,0 +1,51 @@
+#ifndef SOLUTION_SYSTEM_INDICATORS_TEMPLATE_HPP
+#define SOLUTION_SYSTEM_INDICATORS_TEMPLATE_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#  pragma once
+#endif // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include "../indicators.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace indicators
+		{
+			class TEMPLATE
+			{
+			public:
+
+				explicit TEMPLATE(const std::string & name, std::size_t timesteps) : 
+					m_name(name), m_timesteps(timesteps)
+				{
+					initialize();
+				}
+
+			private:
+
+				void initialize() const;
+
+			public:
+
+				void operator()(inputs_container_t & inputs) const;
+
+			private:
+
+				const std::string m_name;
+
+			private:
+
+				std::size_t m_timesteps;
+			};
+
+		} // namespace indicators
+
+	} // namespace system
+
+} // namespace solution
+
+#endif // #ifndef SOLUTION_SYSTEM_INDICATORS_TEMPLATE_HPP
