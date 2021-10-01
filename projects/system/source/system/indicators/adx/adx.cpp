d47f7e8ace5addbe7a27c1e0143fd200f764b27f
Ivan <i.s.m.mipt@yandex.ru>
2021-10-01 13:02:22 +0300

    Добавлена доработанная реализация индикатора ADX.

---
+++ projects/system/source/system/indicators/adx/adx.cpp
@@ -0,0 +1,103 @@
+#include "adx.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace indicators
+		{
+			void ADX::initialize() const
+			{
+				LOGGER(logger);
+
+				try
+				{
+					if (m_timesteps == 0ULL)
+					{
+						throw std::domain_error("required: (timesteps > 0)");
+					}
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < indicator_exception > (logger, exception);
+				}
+			}
+
+			void ADX::operator()(inputs_container_t & inputs) const
+			{
+				LOGGER(logger);
+
+				try
+				{
+					const auto epsilon = std::numeric_limits < double > ::epsilon();
+
+					const auto k = 2.0 / (m_timesteps + 1.0);
+
+					auto pdi = 0.0, ndi = 0.0;
+
+					std::vector < double > dx(std::size(inputs) - 1U, 0.0);
+
+					for (auto i = 1U; i < std::size(inputs); ++i)
+					{
+						auto tr = std::max(epsilon,
+							std::max(inputs[i].price_high, inputs[i - 1U].price_close) -
+							std::min(inputs[i].price_low,  inputs[i - 1U].price_close));
+
+						auto pm = inputs[i     ].price_high - inputs[i - 1U].price_high;
+						auto nm = inputs[i - 1U].price_low  - inputs[i     ].price_low;
+
+						auto pdm = 0.0;
+						auto ndm = 0.0;
+
+						if (pm - nm > epsilon && pm > epsilon)
+						{
+							pdm = pm;
+						}
+
+						if (nm - pm > epsilon && nm > epsilon)
+						{
+							ndm = nm;
+						}
+
+						auto pdm_div_tr = pdm / tr;
+						auto ndm_div_tr = ndm / tr;
+
+						if (i == 1U)
+						{
+							pdi = pdm_div_tr;
+							ndi = ndm_div_tr;
+						}
+						else
+						{
+							pdi = k * pdm_div_tr + (1.0 - k) * pdi;
+							ndi = k * ndm_div_tr + (1.0 - k) * ndi;
+						}
+
+						dx[i - 1U] = 100.0 * std::abs(pdi - ndi) / std::max((pdi + ndi), epsilon);
+					}
+
+					std::vector < double > adx(std::size(dx), 0.0);
+
+					adx.front() = dx.front();
+
+					for (auto i = 1U; i < std::size(adx); ++i)
+					{
+						adx[i] = k * dx[i] + (1.0 - k) * adx[i - 1U];
+					}
+
+					for (auto i = 1U + m_timesteps; i < std::size(inputs); ++i)
+					{
+						inputs[i].ADX[m_name] = adx[i - 1U];
+					}
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < indicator_exception > (logger, exception);
+				}
+			}
+
+		} // namespace indicators
+
+	} // namespace system
+
+} // namespace solution
---
+++ projects/system/source/system/indicators/adx/adx.hpp
@@ -0,0 +1,56 @@
+#ifndef SOLUTION_SYSTEM_INDICATORS_ADX_HPP
+#define SOLUTION_SYSTEM_INDICATORS_ADX_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#  pragma once
+#endif // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include "../indicators.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		namespace indicators
+		{
+			class ADX
+			{
+			public:
+
+				explicit ADX(const std::string & name, std::size_t timesteps) : 
+					m_name(name), m_timesteps(timesteps)
+				{
+					initialize();
+				}
+
+			private:
+
+				void initialize() const;
+
+			public:
+
+				void operator()(inputs_container_t & inputs) const;
+
+			private:
+
+				static inline const auto min =   0.0;
+				static inline const auto max = 100.0;
+
+			private:
+
+				const std::string m_name;
+
+			private:
+
+				std::size_t m_timesteps;
+			};
+
+		} // namespace indicators
+
+	} // namespace system
+
+} // namespace solution
+
+#endif // #ifndef SOLUTION_SYSTEM_INDICATORS_ADX_HPP
