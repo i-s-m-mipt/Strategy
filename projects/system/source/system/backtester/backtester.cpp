9db8c2b66199f53d4724d15a2e0177704384cb4c
MarKovka20 <masha2000_1@mail.ru>
2021-10-05 19:29:12 +0300

    Реализован модуль back-тестирования.

---
+++ projects/system/source/system/backtester/backtester.cpp
@@ -0,0 +1,112 @@
+#include "backtester.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		void Backtester::initialize()
+		{
+			LOGGER(logger);
+
+			try
+			{
+				// ...
+			}
+			catch (const std::exception & exception)
+			{
+				shared::catch_handler < backtester_exception > (logger, exception);
+			}
+		}
+
+		void Backtester::uninitialize()
+		{
+			LOGGER(logger);
+
+			try
+			{
+				// ...
+			}
+			catch (const std::exception & exception)
+			{
+				shared::catch_handler < backtester_exception > (logger, exception);
+			}
+		}
+
+		Backtester::Result Backtester::run() const
+		{
+			LOGGER(logger);
+
+			try
+			{
+				const auto epsilon = std::numeric_limits < double > ::epsilon();
+
+				const auto transaction = m_config.transaction;
+				const auto commission  = m_config.commission;
+
+				Result result;
+
+				result.rewards.reserve(std::size(m_inputs));
+
+				auto reward = 0.0;
+
+				Strategy::State current_state;
+
+				for (auto i = m_config.skipped_timesteps + m_config.timesteps_prehistory;
+					i < std::size(m_inputs) - 1ULL; ++i)
+				{
+					auto current_reward = 0.0;
+
+					inputs_container_t prehistory(
+						std::next(std::begin(m_inputs), i + 1ULL - m_config.timesteps_prehistory),
+						std::next(std::begin(m_inputs), i + 1ULL));
+
+					auto required_state = m_strategy->handle(prehistory, transaction);
+
+					if (required_state.position == 0.0 && current_state.position != 0.0)
+					{
+						current_reward = -commission * (
+							std::abs(current_state.position));
+
+						current_state = required_state;
+					}
+
+					if (required_state.position > 0.0 && current_state.position <= 0.0)
+					{
+						current_reward = -commission * (
+							std::abs(current_state.position) +
+							std::abs(required_state.position));
+
+						current_state = required_state;
+					}
+
+					if (required_state.position < 0.0 && current_state.position >= 0.0)
+					{
+						current_reward = -commission * (
+							std::abs(current_state.position) +
+							std::abs(required_state.position));
+
+						current_state = required_state;
+					}
+
+					auto price_deviation = (m_inputs[i + 1].price_close - 
+						m_inputs[i].price_close) / std::max(m_inputs[i].price_close, epsilon);
+
+					current_reward         += price_deviation * current_state.position;
+					current_state.position += price_deviation * current_state.position;
+
+					reward += current_reward;
+
+					result.rewards.push_back(std::make_pair(m_inputs[i].date_time, reward));
+				}
+
+				return result;
+			}
+			catch (const std::exception & exception)
+			{
+				shared::catch_handler < backtester_exception > (logger, exception);
+			}
+		}
+
+	} // namespace system
+
+} // namespace solution
---
+++ projects/system/source/system/backtester/backtester.hpp
@@ -0,0 +1,107 @@
+#ifndef SOLUTION_SYSTEM_BACKTESTER_HPP
+#define SOLUTION_SYSTEM_BACKTESTER_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#  pragma once
+#endif // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include <climits>
+#include <cmath>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "../strategies/strategies.hpp"
+
+#include "../../config/config.hpp"
+#include "../../detail/detail.hpp"
+#include "../../detail/inputs/inputs.hpp"
+
+#include "logger/logger.hpp"
+
+namespace solution
+{
+	namespace system
+	{
+		class backtester_exception : public std::exception
+		{
+		public:
+
+			explicit backtester_exception(const std::string& message) noexcept :
+				std::exception(message.c_str())
+			{}
+
+			explicit backtester_exception(const char* const message) noexcept :
+				std::exception(message)
+			{}
+
+			~backtester_exception() noexcept = default;
+		};
+
+		class Backtester
+		{
+		public:
+
+			using Date_Time = detail::Date_Time;
+
+			using inputs_container_t = detail::inputs_container_t;
+
+			using Strategy = strategies::Strategy_Base;
+
+		public:
+
+			struct Result
+			{
+				std::vector < std::pair < Date_Time, double > > rewards;
+			};
+
+		public:
+
+			Backtester(const Config & config, 
+				const inputs_container_t & inputs,
+				std::shared_ptr < Strategy > strategy) :
+				m_config(config), m_inputs(inputs), m_strategy(strategy)
+			{
+				initialize();
+			}
+
+			~Backtester() noexcept
+			{
+				try
+				{
+					uninitialize();
+				}
+				catch (...)
+				{
+					// std::abort();
+				}
+			}
+
+		private:
+
+			void initialize();
+
+			void uninitialize();
+
+		public:
+
+			Result run() const;
+
+		private:
+
+			Config m_config;
+
+			inputs_container_t m_inputs;
+
+			std::shared_ptr < Strategy > m_strategy;
+		};
+
+	} // namespace system
+
+} // namespace solution
+
+#endif // #ifndef SOLUTION_SYSTEM_BACKTESTER_HPP
