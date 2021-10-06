87981d00f39c5bf3273cdefae8a8f4d2c10a9389
Ivan <i.s.m.mipt@yandex.ru>
2021-10-06 18:58:13 +0300

    Рефакторинг: удалены лишние комментарии из файла system.cpp.

--- projects/system/source/system/system.cpp
+++ projects/system/source/system/system.cpp
@@ -304,15 +304,15 @@ namespace solution
 
 			try
 			{
-				m_indicators.push_back(indicators::ADX ("default",  14          )); // 1 indicator(s)
-				m_indicators.push_back(indicators::EMA ("6",         6          )); // 1 indicator(s)
-				m_indicators.push_back(indicators::EMA ("12",       12          )); // 1 indicator(s)
-				m_indicators.push_back(indicators::EMA ("48",       48          )); // 1 indicator(s)
-				m_indicators.push_back(indicators::EMA ("288",     288          )); // 1 indicator(s)
-				m_indicators.push_back(indicators::MACD("default",  12,  26,   9)); // 1 indicator(s)
-				m_indicators.push_back(indicators::MACD("slow",     60, 130,  45)); // 1 indicator(s)
-				m_indicators.push_back(indicators::MFI ("default",  14          )); // 1 indicator(s)
-				m_indicators.push_back(indicators::RSI ("default",  14          )); // 1 indicator(s)
+				m_indicators.push_back(indicators::ADX ("default",  14          ));
+				m_indicators.push_back(indicators::EMA ("6",         6          ));
+				m_indicators.push_back(indicators::EMA ("12",       12          ));
+				m_indicators.push_back(indicators::EMA ("48",       48          ));
+				m_indicators.push_back(indicators::EMA ("288",     288          ));
+				m_indicators.push_back(indicators::MACD("default",  12,  26,   9));
+				m_indicators.push_back(indicators::MACD("slow",     60, 130,  45));
+				m_indicators.push_back(indicators::MFI ("default",  14          ));
+				m_indicators.push_back(indicators::RSI ("default",  14          ));
 			}
 			catch (const std::exception & exception)
 			{
