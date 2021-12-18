f72caa5af5a652427f87f6e1fbb631ee986ec0c6
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-12-18 20:19:11 +0300

    Добавлена обновленная реализация индикатора EMA.

--- projects/system/source/source/indicators/ema/ema.cpp
+++ projects/system/source/source/indicators/ema/ema.cpp
@@ -14,20 +14,36 @@ namespace solution
 
 					try
 					{
-						inputs.front().indicators[name] = inputs.front().price_close;
-
-						auto k = 2.0 / (m_config.ema_timesteps + 1.0);
-
-						for (auto i = 1ULL; i < std::size(inputs); ++i)
-						{
-							inputs[i].indicators[name] = k * inputs[i].price_close +
-								(1.0 - k) * inputs[i - 1].indicators[name];
-						}
+						run_implementation(inputs, m_config.ema_timesteps_fast, Key::fast);
+						run_implementation(inputs, m_config.ema_timesteps_slow, Key::slow);
 					}
 					catch (const std::exception & exception)
 					{
 						shared::catch_handler < indicator_exception > (logger, exception);
 					}
+				}
+
+				void EMA::run_implementation(inputs_container_t & inputs,
+					std::size_t timesteps, const std::string & key) const
+				{
+					LOGGER(logger);
+
+					try
+					{
+						inputs.front().indicators[name][key] = inputs.front().price_close;
+
+						auto k = 2.0 / (timesteps + 1.0);
+
+						for (auto i = 1ULL; i < std::size(inputs); ++i)
+						{
+							inputs[i].indicators[name][key] = k * inputs[i].price_close +
+								(1.0 - k) * inputs[i - 1].indicators[name][key];
+						}
+					}
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < indicator_exception > (logger, exception);
 				}
 
 			} // namespace indicators
